c352d202c4c34cbb9f5ae43c15c4973e150d8be0
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:46:47 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,30 @@
+﻿// Вводятся положительные числа. Определите сумму чисел, делящихся на положительное число a нацело.
+// При вводе отрицательного числа закончите работу;
+
+using System;
+
+class work_6_4
+{
+    static void Main()
+    {
+        Console.Write("Введите делитель: "); 
+        int a = Convert.ToInt32(Console.ReadLine()); 
+        int sum = 0; 
+
+        while (true)
+        {
+            Console.Write("Введите число: "); 
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            if ( n < 0)
+            {
+                Console.WriteLine($"Итог: {(sum/a)}"); 
+                break; 
+            }
+            else
+            {
+                sum += n; 
+            }
+        }
+    }
+}
+
